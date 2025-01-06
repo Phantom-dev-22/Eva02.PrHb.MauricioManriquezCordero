b8eb16aa459eb8e86fa412cc22f7ad222208f433
Phantom.Dll <126106454+Phantom-dev-22@users.noreply.github.com>
2025-01-06 15:29:40 -0300

    Delete Eva02.PrHb.MauricioManriquezCorder directory

--- Eva02.PrHb.MauricioManriquezCorder/eva02/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.eva02',
-  appName: 'eva02',
-  webDir: 'www'
-};
-
-export default config;
--- Eva02.PrHb.MauricioManriquezCorder/eva02/src/app/model/cita.ts
+++
@@ -1,6 +0,0 @@
-export class Cita { // Clase Cita
-    constructor(
-        public texto: string = '', // Propiedades de la clase
-        public autor: string = ''  // Propiedades de la clase
-    ) { }
-}
